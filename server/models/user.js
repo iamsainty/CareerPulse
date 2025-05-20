a45f025fd489a06bca66bbf25107fa2b4714921c
Priyanshu Chaurasia <ppriyanshuchaurasia@gmail.com>
2025-05-20 11:14:24 +0530

    user model

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  yearsOfExperience: {
+    type: Number,
+  },
+  skills: {
+    type: [String],
+  },
+  jobType: {
+    type: String,
+    enum: ["Remote", "Hybrid", "On-site"],
+  },
+  createdAt: {
+    type: Date,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
